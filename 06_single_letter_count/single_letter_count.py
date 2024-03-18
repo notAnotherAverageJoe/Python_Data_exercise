3198f9b816f5a14c657972133826e2780ed91bcf
notAnotherAverageJoe <joeskokan20@.com>
2024-03-17 21:37:13 -0400

    finsihed 6

--- 06_single_letter_count/single_letter_count.py
+++ 06_single_letter_count/single_letter_count.py
@@ -10,3 +10,13 @@ def single_letter_count(word, letter):
         >>> single_letter_count("Hello World", 'l')
         3
     """
+    word = word.lower()
+    letter = letter.lower()
+
+    count = 0
+
+    for char in word:
+        if char == letter:
+            count += 1
+    return count
+            
