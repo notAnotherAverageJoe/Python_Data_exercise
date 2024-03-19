b35364cf8a67aeeeb8a5c97b497b0bee9862f909
notAnotherAverageJoe <joeskokan20@.com>
2024-03-18 22:30:17 -0400

    28 done

--- 28_find_factors/find_factors.py
+++ 28_find_factors/find_factors.py
@@ -13,3 +13,11 @@ def find_factors(num):
     >>> find_factors(321421)
     [1, 293, 1097, 321421]
     """
+    factors = []  # Initialize an empty list to store factors
+    for i in range(1, int(num ** 0.5) + 1):  # Iterate up to the square root of num
+        if num % i == 0:
+            factors.append(i)
+            if i != num // i:  # Avoid duplicate factors if num is a perfect square
+                factors.append(num // i)
+    factors.sort()  # Sort the factors in increasing order
+    return factors
