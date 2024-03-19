95ad0b6753e2aef6f30a4907a5ed4411827c6754
notAnotherAverageJoe <joeskokan20@.com>
2024-03-18 23:02:24 -0400

    fixes

--- 34_same_frequency/same_frequency.py
+++ 34_same_frequency/same_frequency.py
@@ -10,3 +10,16 @@ def same_frequency(num1, num2):
         >>> same_frequency(1212, 2211)
         True
     """
+    str_num1 = str(num1)
+    str_num2 = str(num2)
+    
+    # Count the frequency of each digit in both numbers
+    freq_num1 = {}
+    freq_num2 = {}
+    for digit in str_num1:
+        freq_num1[digit] = freq_num1.get(digit, 0) + 1
+    for digit in str_num2:
+        freq_num2[digit] = freq_num2.get(digit, 0) + 1
+    
+    # Compare the frequencies of digits in both numbers
+    return freq_num1 == freq_num2
--- 35_two_oldest_ages/two_oldest_ages.py
+++ 35_two_oldest_ages/two_oldest_ages.py
@@ -21,3 +21,9 @@ def two_oldest_ages(ages):
     # you may find it helpful to research the `sorted(iter)` function, which
     # can take *any* type of list-like-thing, and returns a new, sorted list
     # from it.
+    
+    sorted_ages = sorted(ages, reverse=True)
+    # this finds the second oldest and oldest ages
+    second_oldest = sorted_ages[1]
+    oldest = sorted_ages[0]
+    return (second_oldest, oldest)
