939fa0965ba6ded28c870f9ba58a39a8a0ff706b
notAnotherAverageJoe <joeskokan20@.com>
2024-03-18 22:58:03 -0400

    33 done

--- 33_sum_range/sum_range.py
+++ 33_sum_range/sum_range.py
@@ -23,3 +23,9 @@ def sum_range(nums, start=0, end=None):
         >>> sum_range(nums, 1, 99)
         9
     """
+    if end is None or end > len(nums):
+        end = len(nums)
+    else:
+        end += 1  
+
+    return sum(nums[start:end])
